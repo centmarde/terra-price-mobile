93f677c21dd2b7d848d644af64b6e5ffe6341ec0
BritneySpears <159101935+centmarde@users.noreply.github.com>
2025-09-06 17:09:40 +0800

    update package name and application label to 'terraPrice'

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.nutriplan"
+    namespace = "com.example.terraPrice"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -26,7 +26,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.nutriplan"
+        applicationId = "com.example.terraPrice"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion  // Updated from flutter.minSdkVersion to meet Firebase Auth requirements
